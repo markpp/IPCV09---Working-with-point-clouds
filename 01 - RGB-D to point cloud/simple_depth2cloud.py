e1705f675a9ed8b95f3de791c22c7ca8fc934604
Mark Philip <markpp@gmail.com>
2020-10-12 00:17:07 +0200

    Update simple_depth2cloud.py

--- 01 - RGB-D to point cloud/simple_depth2cloud.py
+++ 01 - RGB-D to point cloud/simple_depth2cloud.py
@@ -4,28 +4,31 @@ import cv2
 import os
 import open3d as o3d
 
-# accuracy can be improved. How?
+# can be improved. How?
 def create_point_cloud(depth,rgb):
     height, width = depth.shape
+    v_0, u_0 = height/2, width/2
+    fx_ = (1/572.4114) # inverse focal length fx
+    fy_ = (1/573.57043) # inverse focal length fy
     points = []
-    for j in range(height):
-        for i in range(width):
+    for v in range(height):
+        for u in range(width):
             # 3D
-            d = depth[j,i]
+            d = depth[v,u]
             z = d
-            x = (i-width/2)*d*(1/572.4114) # inverse focal length fx
-            y = (j-height/2)*d*(1/573.57043) # inverse focal length fy
+            x = d*(u-u_0)*fx_
+            y = d*(v-v_0)*fy_
             # color
-            r = rgb[j,i,0]
-            g = rgb[j,i,1]
-            b = rgb[j,i,2]
+            r = rgb[v,u,0]
+            g = rgb[v,u,1]
+            b = rgb[v,u,2]
             points.append([x,y,z,r,g,b])
 
     return np.array(points)
 
 
 if __name__ == '__main__':
-    rgb = (cv2.cvtColor(cv2.imread("rgb.png"), cv2.COLOR_BGR2RGB)/255.0).astype('float32')
+    rgb = cv2.cvtColor(cv2.imread("rgb.png"), cv2.COLOR_BGR2RGB)/255.0
     depth = cv2.imread("depth.png",-1)/1000.0 # convert to meters
 
     color_points = create_point_cloud(depth,rgb)
@@ -33,4 +36,4 @@ if __name__ == '__main__':
     cloud = o3d.geometry.PointCloud()
     cloud.points = o3d.utility.Vector3dVector(color_points[:,:3])
     cloud.colors = o3d.utility.Vector3dVector(color_points[:,3:])
-    o3d.io.write_point_cloud("simple_cloud.ply", cloud)
+    o3d.io.write_point_cloud("simple_cloud.ply", cloud, write_ascii=True)
